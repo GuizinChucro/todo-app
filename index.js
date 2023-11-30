cee42273d4d0d2321b0d22b6e3856157a251ba46
GuizinChucro <guilherme.mgomes@eaportal.org>
2023-11-30 10:18:22 -0300

    salvando tarefas no banco de dados

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const exphbs = require ("express-handlebars")
+const exphbs = require("express-handlebars")
 const mysql = require("mysql2")
 
 const app = express()
@@ -8,6 +8,32 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+
+//converter dados do formulario em objeto javascript
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
 
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
